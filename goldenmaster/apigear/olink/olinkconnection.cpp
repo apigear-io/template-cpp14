437ad81036e95d40e49fc9621d65e0011378a306
dphan10 <dorota.phan@siili.com>
2022-11-10 16:48:11 +0100

    remove hardcoded payload size

--- goldenmaster/apigear/olink/olinkconnection.cpp
+++ goldenmaster/apigear/olink/olinkconnection.cpp
@@ -57,26 +57,24 @@ void OlinkConnection::receiveInLoop()
     auto serverClosedConnection = false;
     do{
         try {
-            // TODO change buffer size
-            char buffer[1024];
-            std::memset(buffer, 0, sizeof buffer);
+            Poco::Buffer<char> pocobuffer(0);
             int flags;
             auto canSocketRead = m_socket ? m_socket->poll(Poco::Timespan(10000), Poco::Net::WebSocket::SELECT_READ) : false;
             if (canSocketRead && !m_disconnectRequested && m_socket) {
                 std::unique_lock<std::timed_mutex> lock(m_socketMutex);
-                auto frameSize = m_socket->receiveFrame(buffer, sizeof(buffer), flags);
+                auto frameSize = m_socket->receiveFrame(pocobuffer, flags);
                 lock.unlock();
-
+                auto messagePayload = std::string(pocobuffer.begin(), frameSize);
                 auto frameOpCode = flags & Poco::Net::WebSocket::FRAME_OP_BITMASK;
                 if (frameOpCode == Poco::Net::WebSocket::FRAME_OP_PING){
-                    writeMessage(buffer, Poco::Net::WebSocket::FRAME_OP_PONG);
-                } else if (frameOpCode == Poco::Net::WebSocket::FRAME_OP_PONG) {
+                    writeMessage(messagePayload, Poco::Net::WebSocket::FRAME_OP_PONG);
+                }  else if (frameOpCode == Poco::Net::WebSocket::FRAME_OP_PONG) {
                     // handle pong
                 } else if (frameSize == 0 || frameOpCode == Poco::Net::WebSocket::FRAME_OP_CLOSE){
                     std::cout << "close connection" << std::endl;
                     serverClosedConnection = true;
                 } else {
-                    handleTextMessage(buffer);
+                    handleTextMessage(messagePayload);
                 }
             }
         }
